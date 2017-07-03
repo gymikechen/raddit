30664099438227c57d9ebb4ccf1a2a3bc5c7292e
Mike <mike@kkday.com>
2017-07-03 18:42:52 +0800

    l10n

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,7 +9,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @link, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @link, notice: '發布成功！' }
         format.json { render json: @comment, status: :created, location: @comment }
       else
         format.html { render action: "new" }
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -30,7 +30,7 @@ class LinksController < ApplicationController
 
     respond_to do |format|
       if @link.save
-        format.html { redirect_to @link, notice: 'Link was successfully created.' }
+        format.html { redirect_to @link, notice: '發布成功！' }
         format.json { render :show, status: :created, location: @link }
       else
         format.html { render :new }
